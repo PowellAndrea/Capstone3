9cdb933ec778438a98f363ab7d2b61780d5eb1d4
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-06-10 11:47:56 -0700

    docs

---
+++ Documentation/Backups/EditingGrid.cs
@@ -0,0 +1,23 @@
+﻿
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadata_Manager.Models
+{
+
+   public class EditingGrid : DataGridView
+   {
+		// implemented as a singleton
+		public Lazy<EditingGrid> lazy = 
+         new Lazy<EditingGrid>(() => new EditingGrid());
+
+      public EditingGrid Instance { get { return lazy.Value; } }
+
+   }
+}
+// Use fields from the Record object to generate table, instead of addig to .net generated columns.
+// File Name | Title | Year Published | Start Year | End Year| Author | Record Series | File Path
